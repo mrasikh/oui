95ea3731c4d62a15183ce8f277cdc65dc1b0e644
Motasim Rasikh <mrasikh@folio3.com>
2018-10-10 16:30:17 +0500

    separating out SelectDropdown.Option.

--- src/components/SelectDropdown/index.js
+++ src/components/SelectDropdown/index.js
@@ -5,6 +5,8 @@ import classNames from 'classnames';
 import Button from '../Button';
 import Dropdown from '../Dropdown';
 
+import SelectOption from './Option';
+
 class SelectDropdown extends React.Component {
   static propTypes = {
     /**
@@ -145,63 +147,6 @@ class SelectDropdown extends React.Component {
   }
 }
 
-class SelectOption extends React.Component {
-  static propTypes = {
-    /**
-     * Description of select item.
-     */
-    description: PropTypes.string,
-    /** Toggle dropdown open/closed */
-    handleToggle: PropTypes.func,
-    /**
-     * Whether or not item has been selected or not.
-     */
-    isSelected: PropTypes.bool.isRequired,
-    /**
-     * Label of select item.
-     */
-    label: PropTypes.node.isRequired,
-    /**
-     * Function that is called when user selects another item.
-     */
-    onChange: PropTypes.func.isRequired,
-    /**
-     * Value of select item.
-     */
-    value: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-      PropTypes.bool,
-    ]).isRequired,
-  };
-
-  onClick = () => {
-    this.props.onChange(this.props.value);
-  };
-
-  render() {
-    const { isSelected, label, description, value, handleToggle } = this.props;
-    return (
-      <Dropdown.ListItem hideOnClick={ true } handleToggle={ handleToggle }>
-        <Dropdown.BlockLink
-          isLink={ !isSelected }
-          onClick={ this.onClick }
-          testSection={ 'dropdown-block-link-' + value }>
-          { label }
-          { description && (
-            <div className="micro muted">
-              { description }
-            </div>
-          )}
-        </Dropdown.BlockLink>
-      </Dropdown.ListItem>
-    );
-  }
-}
-
-SelectOption.defaultProps = {
-  description: '',
-  handleToggle: () => {},
-};
+SelectDropdown.Option = SelectOption;
 
 export default SelectDropdown;
